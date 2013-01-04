b03425a8b826e166a83dd12e9075c1b8cfd12630
Tomas Petricek <tomas@tomasp.net>
2013-01-04 03:35:34 +0100

    Update publish script

--- nuget/publish.cmd
+++ nuget/publish.cmd
@@ -1,3 +1,4 @@
-nuget pack
-
-nuget push FSharp.Data.1.0.8.nupkg
+nuget pack FSharp.Data.nuspec
+nuget pack FSharp.Data.Experimental.nuspec
+nuget push FSharp.Data.1.0.9.nupkg
+nuget push FSharp.Data.Experimental.1.0.0.nupkg
