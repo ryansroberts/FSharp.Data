2fffa3a8c32ce538c8eeab5e8fd8afd9fad49ba9
Tomas Petricek <tomas@tomasp.net>
2013-01-04 04:17:14 +0100

    Adding experimental Apiary provider

--- nuget/publish.cmd
+++ nuget/publish.cmd
@@ -1,4 +1,2 @@
-nuget pack FSharp.Data.nuspec
 nuget pack FSharp.Data.Experimental.nuspec
-nuget push FSharp.Data.1.0.9.nupkg
-nuget push FSharp.Data.Experimental.1.0.0.nupkg
+nuget push FSharp.Data.Experimental.1.0.1.nupkg
